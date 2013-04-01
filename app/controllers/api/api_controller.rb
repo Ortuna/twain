6040e1cc93ab6e042810d3b3245b55819b0b5786
Sumeet Singh <ortuna@gmail.com>
2013-04-01 11:42:19 -0400

    refactor

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,13 @@
 class Twain::App
 
+  def find_repo(book_id)
+    Repo.first(:id => book_id.to_i) || halt(404)
+  end
+
+  def create_api(repo_location, user)
+    Twain::API.new(git: repo_location, prefix: '/tmp', user: user)
+  end
+
   before do
     if request.path_info[/\A\/api/]
       halt(403, 'login required') unless session[:user]
@@ -11,31 +19,21 @@ class Twain::App
   end
 
   get '/api/book/:book_id' do |book_id|
-    repo = Repo.first(:id => book_id.to_i)
-    halt 404 unless repo
-    @api = Twain::API.new(git:    repo[:location],
-                          prefix: '/tmp',
-                          user:   session[:user])
-
-    @api.current_book.to_json
+    repo = find_repo(book_id)
+    api  = create_api(repo[:location], session[:user])
+    api.current_book.to_json
   end
 
   get '/api/book/:book_id/chapters' do |book_id|
-    repo = Repo.first(:id => book_id.to_i)
-    halt 404 unless repo
-    @api = Twain::API.new(git:    repo[:location],
-                          prefix: '/tmp',
-                          user:   session[:user])
-    @api.current_book.chapters.to_json    
+    repo = find_repo(book_id)
+    api  = create_api(repo[:location], session[:user])
+    api.current_book.chapters.to_json
   end
 
   get '/api/book/:book_id/chapter/:chapter_id/sections' do |book_id, chapter_id|
-    repo = Repo.first(:id => book_id.to_i)
-    halt 404 unless repo
-    @api = Twain::API.new(git:    repo[:location],
-                          prefix: '/tmp',
-                          user:   session[:user])
-    chapter = @api.current_book.chapters.first(:base_path => chapter_id)
+    repo    = find_repo(book_id)
+    api     = create_api(repo[:location], session[:user])
+    chapter = api.current_book.chapters.first(:base_path => chapter_id)
     chapter.sections.to_json
   end
 
