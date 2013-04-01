f828450786757b5cd07fd262047241b7acc74f1b
Sumeet Singh <ortuna@gmail.com>
2013-04-01 10:02:54 -0400

    logout

--- app/controllers/api/authenticate.rb
+++ app/controllers/api/authenticate.rb
@@ -6,6 +6,11 @@ class Twain::App
     user     = authenticate_user(username, password)
     user ? user.to_json : halt(403, 'invalid login')
   end
+
+  get '/logout' do
+    session.clear
+    'logged out'
+  end
   
   private
   def authenticate_user(username, password)
--- spec/api/authenticate_spec.rb
+++ spec/api/authenticate_spec.rb
@@ -4,12 +4,13 @@ describe 'authentication' do
     Helper.clean_users
     Helper.create_user('apiuser', 'apipassword')
 
-    @end_point = '/login'
+    @login_end_point  = '/login'
+    @logout_end_point = '/logout'
   end
 
   it 'authenticates a user correctly' do
     params = { username: 'apiuser', password: 'apipassword' }
-    post @end_point, params
+    post @login_end_point, params
 
     user = Helper.parse_json(last_response.body)
     user["username"].should == params[:username]
@@ -19,13 +20,28 @@ describe 'authentication' do
 
   it 'denies login with invalid username and pass' do
     params = { username: 'apiuser', password: 'apipasswordzzz' }
-    post @end_point, params
+    post @login_end_point, params
 
     last_response.should_not be_ok
   end
 
   it 'denies without params' do
-    post @end_point
+    post @login_end_point
+    last_response.should_not be_ok
+  end
+
+  it 'logs you out when visiting logout end point' do
+    params = { username: 'apiuser', password: 'apipassword' }
+    post @login_end_point, params
+    last_response.should be_ok
+
+    get '/api/book'
+    last_response.should be_ok
+
+    get @logout_end_point
+    last_response.should be_ok
+
+    get '/api/book'
     last_response.should_not be_ok
   end
 end
