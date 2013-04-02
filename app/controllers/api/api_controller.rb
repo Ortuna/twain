02cf72ec22167b0db1569a8017499af9c97bc3e0
Sumeet Singh <ortuna@gmail.com>
2013-04-02 09:47:07 -0400

    Refactor

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,34 @@
 class Twain::App
+  
+  def setup_request(book_id)
+    repo = find_repo(book_id)
+    create_api(repo[:location], session[:user])
+  end
+
+  def setup_api(book_id)
+    @api ||= setup_request(book_id)
+  end
+
+  def book
+    api.current_book
+  end
+
+  def find_section(book_id, chapter_id, section_id)
+    setup_api(book_id)
+    chapter   = book.chapters.first(:base_path => chapter_id)
+    not_found unless chapter
+    
+    chapter.sections.first(:base_path => section_id).tap do |section|
+      not_found unless section
+    end
+  end
+
+  def find_chapter(book_id, chapter_id)
+    setup_api(book_id)
+    api.current_book.chapters.first(:base_path => chapter_id).tap do |chapter|
+      not_found unless chapter
+    end
+  end
 
   before do
     check_login if request.path_info[/\A\/api/]
@@ -9,25 +39,22 @@ class Twain::App
   end
 
   get '/api/book/:book_id' do |book_id|
-    setup_request(book_id)
+    @api = setup_request(book_id)
     api.current_book.to_json
   end
 
   get '/api/book/:book_id/chapters' do |book_id|
-    setup_request(book_id)
+    @api = setup_request(book_id)
     api.current_book.chapters.to_json
   end
 
   get '/api/book/:book_id/chapter/:chapter_id/sections' do |book_id, chapter_id|
-    setup_request(book_id)
-    chapter = api.current_book.chapters.first(:base_path => chapter_id)
-    chapter.sections.to_json
+    find_chapter(book_id, chapter_id).sections.to_json
   end
 
 
   post '/api/book/:book_id' do |book_id|
-    setup_request(book_id)
-    book      = api.current_book
+    setup_api(book_id)
     post_book = parse_json(params["book"])
 
     invalid_entity if !post_book || post_book.empty?
@@ -37,70 +64,47 @@ class Twain::App
   end
 
   post '/api/book/:book_id/chapters' do |book_id|
-    setup_request(book_id)
-    book         = api.current_book
+    setup_api(book_id)
     post_chapter = parse_json(params["chapter"])
 
     invalid_entity if post_chapter.empty?
 
-    chapter = Chapter.new
+    chapter           = Chapter.new
     chapter.base_path = post_chapter["base_path"]
-    chapter.book = book
+    chapter.book      = book
     invalid_entity unless chapter.save
   end
 
   post '/api/book/:book_id/chapter/:chapter_id' do |book_id, chapter_id|
-    setup_request(book_id)
-    book         = api.current_book
+    setup_api(book_id)
     post_chapter = parse_json(params["chapter"])
 
     invalid_entity if !post_chapter || post_chapter.empty?
 
-    chapter   = book.chapters.first(:base_path => post_chapter["base_path"])
-    not_found unless chapter
-
+    chapter = find_chapter(book_id, post_chapter["base_path"])
     update_attributes(chapter, post_chapter)
     chapter.save if chapter.dirty?
   end
 
   delete '/api/book/:book_id/chapter/:chapter_id' do |book_id, chapter_id|
-    setup_request(book_id)
-    book    = api.current_book
+    setup_api(book_id)
     chapter = book.chapters.first(:base_path => chapter_id)
-
     invalid_entity if chapter.destroy == false
   end
 
   post '/api/book/:book_id/chapter/:chapter_id/section/:section_id' do |book_id, chapter_id, section_id|
-
-    setup_request(book_id)
-    book      = api.current_book
-
-    chapter   = book.chapters.first(:base_path => chapter_id)
-    not_found unless chapter
-
+    setup_api(book_id)
+    section      = find_section(book_id, chapter_id, section_id)
     post_section = parse_json(params["section"])
-    invalid_entity unless post_section
-    
-    section = chapter.sections.first(:base_path => section_id)
-    not_found unless section
 
+    invalid_entity unless post_section
     update_attributes(section, post_section)
     section.save if section.dirty?
   end
 
   delete '/api/book/:book_id/chapter/:chapter_id/section/:section_id' do |book_id, chapter_id, section_id|
-
-    repo      = find_repo(book_id)
-    api       = create_api(repo[:location], session[:user])
-    book      = api.current_book
-
-    chapter   = book.chapters.first(:base_path => chapter_id)
-    not_found unless chapter
-   
-    section = chapter.sections.first(:base_path => section_id)
-    not_found unless section
-
+    setup_api(book_id)
+    section = find_section(book_id, chapter_id, section_id)
     invalid_entity if section.destroy == false
   end
 end
--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -27,11 +27,6 @@ class Twain::App
     halt 422
   end
 
-  def setup_request(book_id)
-    repo = find_repo(book_id)
-    @api = create_api(repo[:location], session[:user])
-  end
-
   def update_attributes(model, attributes)
     model.attributes.each do |attribute, value|
       key = attribute.to_s
