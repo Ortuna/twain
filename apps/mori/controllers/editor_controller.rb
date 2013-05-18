70a9d8daf0e6e3446cde2663703563672131f353
Sumeet Singh <ortuna@gmail.com>
2013-05-18 16:12:41 -0400

    basic working ember with assets

--- apps/mori/controllers/editor_controller.rb
+++ apps/mori/controllers/editor_controller.rb
@@ -7,6 +7,27 @@ class Mori::App
     @chapter = @book.chapters.first(base_path: params[:chapter_path])
     @section = @chapter.sections.first(base_path: params[:section_path])
 
-    render :section
+    @js_assets  = js_assets
+    @hbs_assets = handlebar_assets
+    render :editor
   end
+
+  private
+  def js_assets(path = nil)
+    public_assets '**/*.js'
+  end
+
+  def handlebar_assets(path = nil)
+    public_assets '**/*.handlebars'
+  end
+
+  def public_assets(pattern)
+    path ||= Padrino.root('public', 'javascripts', 'app', pattern)
+    [].tap do |paths|
+      Dir.glob(path).each do |asset| 
+        paths << asset.gsub(Padrino.root('public'), '')
+      end
+    end
+  end
+
 end
--- public/javascripts/app/editor.js
+++ public/javascripts/app/editor.js
@@ -1,62 +1,62 @@
-  var editor = ace.edit("markdown");
-  editor.setTheme("ace/theme/tomorrow_night");
-  editor.getSession().setUseWorker(false);  
-  editor.getSession().setMode("ace/mode/markdown");
-  editor.getSession().setTabSize(2);
-  editor.getSession().setUseWrapMode(true);
+  // var editor = ace.edit("markdown");
+  // editor.setTheme("ace/theme/tomorrow_night");
+  // editor.getSession().setUseWorker(false);  
+  // editor.getSession().setMode("ace/mode/markdown");
+  // editor.getSession().setTabSize(2);
+  // editor.getSession().setUseWrapMode(true);
 
-  marked.setOptions({
-    gfm: true,
-    tables: true,
-    breaks: true,
-    pedantic: false,
-    sanitize: false,
-    smartLists: true,
-  });
+  // marked.setOptions({
+  //   gfm: true,
+  //   tables: true,
+  //   breaks: true,
+  //   pedantic: false,
+  //   sanitize: false,
+  //   smartLists: true,
+  // });
 
-  editor.getSession().on('change', function(e) {
-    updatePreview(editor.getValue());
-  });
+  // editor.getSession().on('change', function(e) {
+  //   updatePreview(editor.getValue());
+  // });
 
 
-  var $markdown  = null;
-  var $preview = null;
-  var $gutter  = null;
-  var padding  = 0;
+  // var $markdown  = null;
+  // var $preview = null;
+  // var $gutter  = null;
+  // var padding  = 0;
 
-  $(window).resize(function(){
-    windowResized();
-  });
+  // $(window).resize(function(){
+  //   windowResized();
+  // });
 
-  function windowResized(){
-    var height  = $(window).height() - padding;
-    updatePaneHeights(height);
-  }
+  // function windowResized(){
+  //   var height  = $(window).height() - padding;
+  //   updatePaneHeights(height);
+  // }
 
-  function updatePaneHeights(height) {
-    $preview.height(height);
-    $markdown.height(height);
-    $gutter.height(height);
-  }
+  // function updatePaneHeights(height) {
+  //   $preview.height(height);
+  //   $markdown.height(height);
+  //   $gutter.height(height);
+  // }
 
-  function updatePreview(code){
-    var markdown = code.match(/-{3,}\s([\s\S]*?)-{3,}\s([\s\S]*)/)
-    if(markdown == null)
-      markdown = code
-    else
-      markdown = markdown[2]
+  // function updatePreview(code){
+  //   var markdown = code.match(/-{3,}\s([\s\S]*?)-{3,}\s([\s\S]*)/)
+  //   if(markdown == null)
+  //     markdown = code
+  //   else
+  //     markdown = markdown[2]
 
-    $preview.html( marked(code) );
-  }
+  //   $preview.html( marked(code) );
+  // }
 
-  $(document).ready(function(){
-    $markdown = $(".markdown");
-    $preview  = $(".preview");
-    $gutter   = $(".gutter");
+  // $(document).ready(function(){
+  //   $markdown = $(".markdown");
+  //   $preview  = $(".preview");
+  //   $gutter   = $(".gutter");
 
-    padding = $preview.css('padding').match(/\d+/)[0];
+  //   padding = $preview.css('padding').match(/\d+/)[0];
 
-    updatePreview( editor.getValue() );
-    windowResized();
+  //   updatePreview( editor.getValue() );
+  //   windowResized();
    
-  });
+  // });
